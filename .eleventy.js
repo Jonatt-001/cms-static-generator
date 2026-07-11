57e45a888c9469fc1a9dec9f50fe9f5107297ccb
Jonatt-001 <1jonatttech@gmail.com>
2026-07-12 00:24:37 +0100

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,23 @@
 const { DateTime } = require("luxon");
+
 module.exports = function(eleventyConfig) {
+  // Copy static folders
   eleventyConfig.addPassthroughCopy("admin");
   eleventyConfig.addPassthroughCopy("public");
+
+  // --- THIS IS THE FIX ---
+  // Automatically groups all .md files in content/articles into the "articles" collection
+  eleventyConfig.addCollection("articles", function(collectionApi) {
+    return collectionApi.getFilteredByGlob("./content/articles/*.md");
+  });
+  // -----------------------
+
+  // Date filters
   eleventyConfig.addFilter("readableDate", d => DateTime.fromJSDate(d, {zone: 'utc'}).toFormat("LLLL d, yyyy"));
   eleventyConfig.addFilter("htmlDate", d => DateTime.fromJSDate(d, {zone: 'utc'}).toFormat('yyyy-LL-dd'));
-  return { dir: { input: ".", output: "_site" } };
+  
+  return { 
+    dir: { input: ".", output: "_site" } 
+  };
 };
 
